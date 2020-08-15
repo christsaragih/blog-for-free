19bfcc3cda506cb69cc65eb5e9b46b8ee61f5538
solafide.suyono <solafide.suyono@indocyber.co.id>
2020-08-15 17:23:49 +0700

    migration table users and roles

--- database/migrations/2020_08_15_101701_create_roles_table.php
+++ database/migrations/2020_08_15_101701_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('roleId');
-            $table->string('firstName', 50);
-            $table->string('middleName', 50);
-            $table->string('lastName', 50);
-            $table->string('email', 50)->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            $table->string('passwordHash');
-            $table->mediumText('intro');
-            $table->text('profile');
-            $table->rememberToken();
+            $table->string('title', 75);
+            $table->string('slug', 100);
+            $table->mediumText('description');
+            $table->text('content');
+            $table->boolean('active');
             $table->timestamps();
             $table->softDeletes();
             $table->bigInteger('created_by')->nullable();
@@ -40,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 }
